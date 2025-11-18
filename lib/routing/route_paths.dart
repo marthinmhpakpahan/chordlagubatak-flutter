d52239522c73ba1128d52076105ab1d984ddf821
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2025-11-19 00:32:18 +0700

    Routing

---
+++ lib/routing/route_paths.dart
@@ -0,0 +1,12 @@
+class RoutePaths {
+  static const home = '/';
+  static const songDetail = '/song/:id';
+
+  static const login = '/login';
+  static const register = '/register';
+
+  static const favorites = '/favorites';
+
+  static const requestSong = '/request/song';
+  static const requestUpdate = '/request/update/:id';
+}
